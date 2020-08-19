770e295f63830ecea597bc41b70ffe989d6eab1a
vamsi krish <vamsikrishchiguruwada@gmail.com>
2020-08-19 21:34:42 +0530

    Updates

---
+++ precache-manifest.4181f43f50305309cd5e368692d2b457.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad5b5c5682f150e3050a5dc816aa6ed",
+    "url": "/Portfolio-v1/index.html"
+  },
+  {
+    "revision": "45bb9afcfce95766f44f",
+    "url": "/Portfolio-v1/static/css/main.9a0db482.chunk.css"
+  },
+  {
+    "revision": "ace1ad928b7e60a6ca5c",
+    "url": "/Portfolio-v1/static/js/2.380d3a77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-v1/static/js/2.380d3a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bb9afcfce95766f44f",
+    "url": "/Portfolio-v1/static/js/main.615e523e.chunk.js"
+  },
+  {
+    "revision": "8d8d7ba291ee60d5a16e",
+    "url": "/Portfolio-v1/static/js/runtime-main.76bda221.js"
+  },
+  {
+    "revision": "17ff83c9d9d8eec7725bbdeac698115c",
+    "url": "/Portfolio-v1/static/media/about-p1.17ff83c9.jpg"
+  },
+  {
+    "revision": "22dc9de01d9e6c4f0e70f532451e0541",
+    "url": "/Portfolio-v1/static/media/logo_white_large.22dc9de0.png"
+  },
+  {
+    "revision": "49196e07faca3361145065dc680c2f75",
+    "url": "/Portfolio-v1/static/media/p1.49196e07.jpg"
+  },
+  {
+    "revision": "b8694353cac50030e60157dfc3753bba",
+    "url": "/Portfolio-v1/static/media/p2.b8694353.jpg"
+  },
+  {
+    "revision": "338cdcb43c6dce5404d5ecb2375cb71b",
+    "url": "/Portfolio-v1/static/media/p3.338cdcb4.jpg"
+  },
+  {
+    "revision": "1ae25fec0eeb3e40a247dcfd56dabdf0",
+    "url": "/Portfolio-v1/static/media/pic1.1ae25fec.png"
+  }
+]);
